dd54be0c598da5b1d42b3dc27b6aa8fe447dea77
kristyan <kristyancarvalho@hotmail.com>
2026-08-31 04:05:54 -0300

    feat(model): define download state transitions

---
+++ internal/model/errors.go
@@ -0,0 +1,36 @@
+package model
+
+import "fmt"
+
+type InvalidDownloadIDError struct {
+	Value string
+}
+
+func (err InvalidDownloadIDError) Error() string {
+	return fmt.Sprintf("invalid download identifier %q", err.Value)
+}
+
+type InvalidStatusError struct {
+	Value string
+}
+
+func (err InvalidStatusError) Error() string {
+	return fmt.Sprintf("invalid download status %q", err.Value)
+}
+
+type InvalidPriorityError struct {
+	Value string
+}
+
+func (err InvalidPriorityError) Error() string {
+	return fmt.Sprintf("invalid download priority %q", err.Value)
+}
+
+type InvalidTransitionError struct {
+	From Status
+	To   Status
+}
+
+func (err InvalidTransitionError) Error() string {
+	return fmt.Sprintf("invalid download transition from %q to %q", err.From, err.To)
+}
